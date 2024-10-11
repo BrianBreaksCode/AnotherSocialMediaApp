46a210213fe7258e86960358bda775094b16477d
Brian van Batenburg <55662360+BrianBreaksCode@users.noreply.github.com>
2024-10-11 19:10:58 +0200

    Flask Test

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    random_number = random.randint(1, 10)
+    return render_template('index.html', random_number=random_number)
 
 app.run(host="0.0.0.0", port=8008, debug=True)
